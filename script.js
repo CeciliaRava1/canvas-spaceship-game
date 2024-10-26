0ecccf300420dea872f1293546b0d17df81d97e4
Cecilia <ravacecilia1@gmail.com>
2024-10-26 08:49:07 -0300

    feat: Add a cover to the game

--- script.js
+++ script.js
@@ -1,7 +1,97 @@
 game = {
     canvas: null,
-    ctx: null
+    ctx: null,
+    image: null,
+    cover: true,
+    enemyImage: null,
+    keyPressed: null,
+    key: [],
+    shootingColor: 'red',
+    shootingArray: new Array(),
+    enemyArray: new Array(),
+
+};
+
+const keyEnter = 13;
+const keyLeft = 37;
+const keyUp = 38;
+const keyRight = 39;
+const keyDown = 40;
+const spacebar = 32;
+
+
+class Shooting {
+    constructor(x, y, width) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.draw = function () { };
+    };
+};
+
+class Player {
+    constructor(x) {
+        this.x = x;
+        this.y = 450;
+        this.draw = function (x) {
+            this.x = x;
+            game.ctx.drawImage(game, image, this.x, this.y, 30, 15);
+        };
+    };
+};
+
+class Enemy {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this. width = 35;
+        this.amountOfEnemies = 0;
+        this.distanceX = 5; // Speed
+        this.cycles = 0;
+        this.number = 14;
+        this.figure = true;
+        this.live = true;
+        this.draw = function () {};
+    };
+};
+
+const cover = () => {
+    let image = new Image();
+    image.src = 'img/cover.png';
+    image.onload = () => {
+        // Calculate scale
+        const canvasWidth = canvas.width;
+        const canvasHeight = canvas.height;
+
+        // Image proportions
+        const imgWidth = image.width;
+        const imgHeight = image.height;
+
+        // Calculate new scale
+        const scale = Math.min(canvasWidth / imgWidth, canvasHeight / imgHeight);
+        const newWidth = imgWidth * scale;
+        const newHeight = imgHeight * scale;
+
+        // Calculate center position
+        const x = (canvasWidth - newWidth) / 2;
+        const y = (canvasHeight - newHeight) / 2;
+
+        game.ctx.drawImage(image, x, y, newWidth, newHeight);
+    };
+};
+
+const select = (e) => {
+    if(game.cover) {
+        start();
+    };
+}; 
+
+const start = () => {
+    game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    game.cover = false;
 }
+
+
 
 let positionX = 100, positionY = 100;
 
@@ -11,13 +101,13 @@ const animate = () => {
     verifyPosition();
     draw();
 
-}
+};
 
 const verifyPosition = () => {
     positionX += 2;
     if(positionX > game.canvas.width) positionX = 0;
 
-}
+};
 
 const draw = () => {
     game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
@@ -26,7 +116,7 @@ const draw = () => {
     game.ctx.arc(positionX, positionY, 5, 0, 2 * Math.PI);
     game.ctx.fill();
 
-}
+};
 
 
 
@@ -45,14 +135,15 @@ window.onload = function(){
         game.ctx = canvas.getContext('2d');
     
         if(game.ctx){
-            animate();
+            cover();
+            // animate();
 
 
         } else{
             alert('Your browser does not support canvas')
-        }
+        };
     
     
     
-    }
-}
+    };
+};
